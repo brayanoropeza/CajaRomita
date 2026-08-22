bc977ea6b6252fe5025e63f59c8295e5973dce24
brayanoropeza <brayanoropeza@gmail.com>
2026-08-21 18:50:53 -0600

    Actualizacion automatica de Caja Romita 21/08/2026 18:50:53.19

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.romita.caja"
         minSdk = 24
         targetSdk = 35
-        versionCode = 11
-        versionName = "2.0"
+        versionCode = 12
+        versionName = "2.1"
     }
 
     buildTypes {
